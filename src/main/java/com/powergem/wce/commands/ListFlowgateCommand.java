3f4f76cdb74b2f9e40117143fbcb1e0cd3a914e8
Michael Buck <mbuck@power-gem.com>
2025-06-18 15:47:31 -0400

    Added new options (`-i`, `-s`, and `-na`), and improved ANSI handling for List Flowgate command.

--- src/main/java/com/powergem/wce/commands/ListFlowgateCommand.java
+++ src/main/java/com/powergem/wce/commands/ListFlowgateCommand.java
@@ -14,25 +14,29 @@ import static com.powergem.wce.Utils.getConnection;
 
 @CommandLine.Command(name = "flowgate")
 public final class ListFlowgateCommand implements Callable<Integer> {
-  @CommandLine.Parameters(index = "0", description = "The JSON file to inspect.", defaultValue = "WClusterTrLimSumJson.json")
-  private Path jsonFile;
-
-  @CommandLine.Parameters(index = "1", description = "The id of the flowgate to list.")
+  @CommandLine.Parameters(index = "0", description = "The id of the flowgate to list.")
   private int flowgateId;
+
+  @CommandLine.Option(names = {"-i", "--input"}, description = "The JSON file to inspect.", defaultValue = "WClusterTrLimSumJson.json")
+  private Path jsonFile = Path.of("WClusterTrLimSumJson.json");
+
+  @CommandLine.Option(names = {"-s", "--scenario"}, description = "The ID of the scenario to get the bus from.", defaultValue = "1")
+  private int scenarioId = 1;
+
+  @CommandLine.Option(names = {"-na", "--no-ansi"}, description = "Do not use ANSI codes in the output")
+  private boolean noAnsi = false;
 
   @Override
   public Integer call() throws Exception {
-    String jdbcUrl = "jdbc:sqlite::memory:";
+    System.setProperty("wce.useAnsi", String.valueOf(!noAnsi));
 
-    int scenarioId = 1;
+    String jdbcUrl = "jdbc:sqlite::memory:";
 
     try (Connection connection = getConnection(jdbcUrl)) {
       Importer.importData(this.jsonFile, connection);
 
       DataFile dataFile = new DataFile(new UncheckedConnection(connection));
-      dataFile.getFlowgateById(this.flowgateId, scenarioId).ifPresent(entity -> {
-        dumpFlowgate(entity, dataFile, scenarioId, 0);
-      });
+      dataFile.getFlowgateById(this.flowgateId, scenarioId).ifPresent(entity -> dumpFlowgate(entity, dataFile, scenarioId, 0));
     }
 
     return 0;
